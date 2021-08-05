0316628d9f2f95005b6cd1f3b8713193da7c55be
Alan <yiukaiyeung1990@gmail.com>
2021-08-05 23:23:23 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:travel_flutter/blocs/internet_bloc.dart';
+import 'package:travel_flutter/blocs/sign_in_bloc.dart';
+
+import 'package:travel_flutter/pages/splash.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(EasyLocalization(
+    supportedLocales: [Locale('en'), Locale('zh')],
+    path: 'assets/translations',
+    fallbackLocale: Locale('en'),
+    //startLocale: Locale('en'),
+    useOnlyLangCode: true,
+    child: TravelFlutterApp(),
+  ));
+}
+
+class TravelFlutterApp extends StatefulWidget {
+  const TravelFlutterApp({Key key}) : super(key: key);
+
+  @override
+  _TravelFlutterAppState createState() => _TravelFlutterAppState();
+}
+
+class _TravelFlutterAppState extends State<TravelFlutterApp> {
+  @override
+
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<InternetBloc>(
+          create: (context) => InternetBloc(),
+        ),
+        ChangeNotifierProvider<SignInBloc>(
+          create: (context) => SignInBloc(),
+        ),
+      ],
+      child: MaterialApp(
+          supportedLocales: EasyLocalization.of(context).supportedLocales,
+          localizationsDelegates: [EasyLocalization.of(context).delegate],
+          locale: EasyLocalization.of(context).locale,
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              primaryColor: Colors.blueAccent,
+              iconTheme: IconThemeData(color: Colors.grey[900]),
+              fontFamily: 'Manrope',
+              scaffoldBackgroundColor: Colors.grey[100],
+              appBarTheme: AppBarTheme(
+                  color: Colors.white,
+                  elevation: 0,
+                  iconTheme: IconThemeData(
+                    color: Colors.grey[800],
+                  ),
+                  brightness:
+                      Platform.isAndroid ? Brightness.dark : Brightness.light,
+                  textTheme: TextTheme(
+                      headline6: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Manrope',
+                          color: Colors.grey[900])))),
+          debugShowCheckedModeBanner: false,
+          home: SplashPage()),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,88 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+// import 'package:travel_flutter/blocs/ads_bloc.dart';
+// import 'package:travel_flutter/blocs/notification_bloc.dart';
+// import 'package:travel_flutter/pages/blogs.dart';
+// import 'package:travel_flutter/pages/bookmark.dart';
+// import 'package:travel_flutter/pages/explore.dart';
+import 'package:travel_flutter/pages/profile.dart';
+// import 'package:provider/provider.dart';
+// import 'package:travel_flutter/pages/states.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _currentIndex = 0;
+  PageController _pageController = PageController();
+
+  List<IconData> iconList = [
+    Feather.home,
+    Feather.grid,
+    Feather.list,
+    Feather.bookmark,
+    Feather.user
+  ];
+
+
+  void onTabTapped(int index) {
+    setState(() {
+     _currentIndex = index;
+     
+   });
+   _pageController.animateToPage(index,
+      curve: Curves.easeIn,
+      duration: Duration(milliseconds: 350));
+   
+  }
+
+ @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 0))
+    .then((value) async{
+
+    });
+  }
+
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeColor: Theme.of(context).primaryColor,
+        gapLocation: GapLocation.none,
+        activeIndex: _currentIndex,
+        inactiveColor: Colors.grey[800],
+        onTap: (index) => onTabTapped(index),
+      ),
+      body: PageView(
+        controller: _pageController,
+
+        physics: NeverScrollableScrollPhysics(),  
+        children: <Widget>[
+          // Explore(),
+          // StatesPage(),
+          // BlogPage(),
+          // BookmarkPage(),
+          ProfilePage(),
+        ],
+      ),
+    );
+  }
+}
